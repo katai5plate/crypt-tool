2f09a35fd2b67a96fb2ac7de208c835463dda4d2
Hadhad / HanoHano <katai5plate.sub@gmail.com>
2019-09-12 18:16:25 +0900

    Update endec.js

--- endec.js
+++ endec.js
@@ -1,5 +1,2 @@
-const contents = () => document.getElementById("contents").value;
-const key = () => document.getElementById("key").value;
-const result = document.getElementById("result");
-window.enc = () => (result.innerText = CryptoJS.AES.encrypt(contents(), key()).toString());
-window.dec = () => (result.innerText = CryptoJS.AES.decrypt(contents(), key()).toString(CryptoJS.enc.Utf8));
+window.enc = (contents, key) => CryptoJS.AES.encrypt(contents, key).toString();
+window.dec = (contents, key) => CryptoJS.AES.decrypt(contents, key).toString(CryptoJS.enc.Utf8);
